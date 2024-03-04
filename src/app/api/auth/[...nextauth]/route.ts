a3ebc8d62c9a2edd19bacd7167217c14031fe120
wdcs-niravjogiya <nirav.jogiya@codezeros.com>
2024-03-04 14:50:27 +0530

    Refactored code

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,3 @@
-import { signIn } from "next-auth/react";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
@@ -17,21 +16,14 @@ const handler = NextAuth({
       // name: "credentials",
       type: "credentials",
       credentials: {},
-      async authorize(credentials: MyCredentials) {
+      async authorize(credentials: any) {
         const { email }: any = credentials;
         if (!email) {
           return null;
         }
         try {
           const authResponse = await fetch(
-            `https://jsonplaceholder.typicode.com/users?email=${email}`,
-            {
-              // method: "POST",
-              // headers: {
-              //   "Content-Type": "application/json",
-              // },
-              // body: JSON.stringify(credentials),
-            }
+            `https://jsonplaceholder.typicode.com/users?email=${email}`
           );
           console.log("API Response Status:", authResponse.status);
 
@@ -45,7 +37,7 @@ const handler = NextAuth({
             return null;
           }
           console.log("user-------------------", user);
-          return { email: user[0].email };
+          return user;
         } catch (error) {
           console.error("Error fetching user data:", error);
           return null;
@@ -53,53 +45,7 @@ const handler = NextAuth({
       },
     }),
   ],
-  // callbacks: {
-  //   async session({ session, user, token }) {
-  //     console.log("callbacks session----------------", session.user?.email);
-  //     return session;
-  //   },
-  // },
   secret: process.env.NEXTAUTH_SECRET,
 });
-
-// const handler = NextAuth({
-//   session: {
-//     strategy: "jwt",
-//   },
-//   pages: {
-//     // signIn: "/",
-//   },
-//   providers: [
-//     CredentialsProvider({
-//       type: "credentials",
-//       credentials: {
-//         email: {
-//           label: "E-Mail",
-//           type: "email",
-//           placeholder: "Enter your Email",
-//         },
-//         // password: { label: "Password", type: "password" },
-//       },
-//       async authorize(credentials) {
-//         // Add logic here to look up the user from the credentials supplied
-//         const { email }: any = credentials;
-
-//         if (email === "nirav@gmail.com") {
-//           // Any object returned will be saved in `email` property of the JWT
-//           return {
-//             Id: 1,
-//             email: email,
-//             UserName: "Nirav",
-//           };
-//         } else {
-//           // If you return null then an error will be displayed advising the user to check their details.
-//           return null;
-
-//           // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-//         }
-//       },
-//     }),
-//   ],
-// });
 
 export { handler as GET, handler as POST };
